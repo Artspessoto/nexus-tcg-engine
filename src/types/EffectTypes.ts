5d4bc24a483093ee09ceb93339cf54c01a52f1c2
Arthur Spessoto <artspessoto@gmail.com>
2026-02-22 03:26:27 -0300

    feat: add new types file for effect types to apply in card object

---
+++ src/types/EffectTypes.ts
@@ -0,0 +1,28 @@
+export type EffectTargetSide = "OWNER" | "OPPONENT" | "BOTH";
+
+interface NumericEffect {
+  type: "BURN" | "HEAL" | "BOOST_ATK" | "NERF_ATK" | "GAIN_MANA";
+  value: number;
+  targetSide: EffectTargetSide;
+}
+
+interface ActionEffect {
+  //"BOUNCE" return card to card owner hand
+  type:
+    | "DESTROY_MONSTER"
+    | "DESTROY_SPELL"
+    | "DESTROY_TRAP"
+    | "CHANGE_POS"
+    | "REVIVE"
+    | "BOUNCE";
+  targetSide: EffectTargetSide;
+  value?: number;
+}
+
+interface UtilityEffect {
+  type: "PROTECT" | "NEGATE" | "DRAW_CARDS";
+  value?: number;
+  targetSide?: EffectTargetSide;
+}
+
+export type CardEffect = NumericEffect | ActionEffect | UtilityEffect;
