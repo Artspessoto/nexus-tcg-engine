116fbd05d24905ac8156f412631dc2fdd3c6dc1e
Arthur Spessoto <artspessoto@gmail.com>
2026-03-22 22:55:29 -0300

    feat: add new type Slot and Move to use into strategy class

--- src/types/GameTypes.ts
+++ src/types/GameTypes.ts
@@ -1,4 +1,5 @@
 import { TRANSLATIONS } from "../constants/Translations";
+import type { Card } from "../objects/Card";
 
 export type Difficulty = "EASY" | "MEDIUM" | "HARD";
 export type GamePhase = "DRAW" | "MAIN" | "BATTLE" | "CHANGE_TURN";
@@ -10,3 +11,21 @@ export type GameSide = "PLAYER" | "OPPONENT";
 export type Lang = "pt-br" | "en";
 export type Notice = "PHASE" | "WARNING" | "TURN" | "NEUTRAL";
 export type BattleTranslations = TranslationStructure["battle_scene"];
+
+export type Slot = {
+  index: number;
+  x: number;
+  y: number;
+};
+
+export type Move =
+  | { type: "PLAY_MONSTER"; card: Card; slot: Slot; mode: "ATK" | "DEF" }
+  | {
+      type: "PLAY_SPELL";
+      card: Card;
+      slot: Slot;
+      mode: "FACE_UP" | "SET";
+    }
+  | { type: "ACTIVATE_EFFECT"; card: Card; target?: Card }
+  | { type: "ATTACK"; attacker: Card; target?: Card }
+  | { type: "PASS" };
