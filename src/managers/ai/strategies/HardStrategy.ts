aefc22645939535b89446943012412c812619eef
Arthur Spessoto <artspessoto@gmail.com>
2026-04-02 02:41:35 -0300

    feat: setup initial strategy model for hard difficulty

--- src/managers/ai/strategies/HardStrategy.ts
+++ src/managers/ai/strategies/HardStrategy.ts
@@ -1,9 +1,11 @@
 import type { IAIStrategy } from "../../../interfaces/IAIStrategy";
 import type { IBattleContext } from "../../../interfaces/IBattleContext";
-import type { Move } from "../../../types/GameTypes";
+import type { Card } from "../../../objects/Card";
+import type { GameSide, Move } from "../../../types/GameTypes";
 
 export class HardStrategy implements IAIStrategy {
   public readonly context: IBattleContext;
+  public readonly side: GameSide = "OPPONENT";
 
   constructor(context: IBattleContext) {
     this.context = context;
@@ -14,5 +16,28 @@ export class HardStrategy implements IAIStrategy {
     return [];
   }
 
+  public mainPhaseAvailableMoves(playableCards: Card[]): Move[] {
+    console.log(playableCards);
+    return [];
+  }
+
+  public battlePhaseAvailableMoves(): Move[] {
+    return [];
+  }
+
   public async playBattlePhase(): Promise<void> {}
+
+  public evaluateMove(move: Move): number {
+    console.log(move);
+    return 1;
+  }
+
+  public async executeMove(move: Move): Promise<void> {
+    console.log(move);
+    await this.delay(200);
+  }
+
+  public async delay(ms: number): Promise<Phaser.Time.TimerEvent> {
+    return new Promise((resolve) => this.context.time.delayedCall(ms, resolve));
+  }
 }
